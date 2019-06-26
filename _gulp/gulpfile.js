4c1a634612b6624d09f4e6774e333e7cf189ad6b
kurobe <kurobe@rio.ne.jp>
2019-06-26 18:07:45 +0900

    change directory name

--- _gulp/gulpfile.js
+++ _gulp/gulpfile.js
@@ -3,11 +3,11 @@ var gulp = require('gulp');
 var tinyping = require('gulp-tinypng-compress');
 
 gulp.task('imagemin', function () {
-    gulp.src('./images/*.{png,jpg,jpeg}')
+    gulp.src('./src/*.{png,jpg,jpeg}')
         .pipe(tinyping({
             key: '1xlxGPO6tzE49mCcx6vNFqEhRNe1iC4Z' // TinyPNGのAPI Key
         }))
-        .pipe(gulp.dest('./optimized_images'));
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['tinypng']);

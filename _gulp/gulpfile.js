a31e89cedc0d1a7731fbd4880c478dd526d3e0e0
kurobe <kurobe@rio.ne.jp>
2019-06-27 11:27:39 +0900

    Tinypingは一度の制限、月制限があるため廃止

--- _gulp/gulpfile.js
+++ _gulp/gulpfile.js
@@ -1,13 +1,47 @@
-// gulpfile.js
-var gulp = require('gulp');
-var tinyping = require('gulp-tinypng-compress');
-
-gulp.task('imagemin', function () {
-    gulp.src('./src/*.{png,jpg,jpeg}')
-        .pipe(tinyping({
-            key: '1xlxGPO6tzE49mCcx6vNFqEhRNe1iC4Z' // TinyPNGのAPI Key
-        }))
-        .pipe(gulp.dest('./dist'));
+/**
+ *
+ * ディレクトリ
+ *
+ ├── dist
+ ├── gulpfile.babel.js
+ ├── package.json
+ └── src
+ */
+const gulp = require('gulp'),
+      docs = '.',
+      distDir =  docs + '/dist',
+      srcDir =  docs + '/src',
+      imagemin = require('gulp-imagemin'),
+      pngquant = require('imagemin-pngquant'),  // 圧縮率を高めるのにプラグインを入れる png
+      mozjpeg = require('imagemin-mozjpeg'),  // 圧縮率を高めるのにプラグインを入れる jpg
+      changed = require('gulp-changed'),
+      notify = require('gulp-notify');
+/**
+ *
+ * Compress and save the image.
+ * Reload the browser.
+ *
+ * 画像を圧縮して保存。
+ * ブラウザを再起動する。
+ *
+ */
+gulp.task('images', () => {
+  return gulp.src(srcDir + '/**/*.{png,jpg,gif,svg}')
+    .pipe(changed(distDir))  // src と dist を比較して異なるものだけ処理
+    .pipe(imagemin([
+      pngquant({
+        quality: '65-80',  // 画質
+        speed: 1,  // 最低のスピード
+        floyd: 0,  // ディザリングなし
+      }),
+      mozjpeg({
+        quality: 85, // 画質
+        progressive: true
+      }),
+      imagemin.svgo(),
+      imagemin.optipng(),
+      imagemin.gifsicle()
+    ]))
+    .pipe(gulp.dest(distDir))  // 保存
+    .pipe(notify('&#x1f363; images task finished &#x1f363;'));
 });
-
-gulp.task('default', ['tinypng']);

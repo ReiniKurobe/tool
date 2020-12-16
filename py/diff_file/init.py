f7b1a7c63e3cfe1989d313db87a7db51e241c954
kurobe <kurobe@rio.ne.jp>
2020-12-16 19:02:13 +0900

    納品ファイル作成ツールを追加

---
+++ py/diff_file/init.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import shutil
+
+# ----------------------------------------
+# 定義
+# ----------------------------------------
+
+diff_file_path = './diff.txt'
+
+# ----------------------------------------
+# diffファイルの存在を確認
+# ----------------------------------------
+
+if os.path.isfile(diff_file_path) == False :
+  print('diff file is not')
+  sys.exit(1)
+
+# ----------------------------------------
+# diffファイルを読み込み
+# ----------------------------------------
+
+# diffファイルを開く
+diff_f = open(diff_file_path)
+diff_file = diff_f.read()
+diff_f.close()
+
+# diffを改行で区切ってファイルごとに独立させる
+diff_list = diff_file.split('\n')
+
+# ----------------------------------------
+# ファイルを探してコピーする
+# ----------------------------------------
+
+for target in diff_list :
+  directory = target.split('/')
+  file_name = directory.pop()
+  directory = './dist/' + '/'.join(directory)
+
+  if os.path.exists(directory) :
+    if os.path.isfile(target) :
+      shutil.copyfile(target, directory + '/' + file_name)
+  else :
+    if os.path.isfile(target) :
+      os.makedirs(directory)
+      shutil.copyfile(target, directory + '/' + file_name)
---
+++ py/diff_file_for_git/init.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import shutil
+import git
+
+# ----------------------------------------
+# リポジトリ情報
+# ----------------------------------------
+
+repo =  git.Repo('./')
+t = repo.head.commit.tree
+diff = repo.git.diff('HEAD~1..HEAD', name_only=True) 
+diff_list = diff.split('\n')
+print(diff_list)
+
+# ----------------------------------------
+# ファイルを探してコピーする
+# ----------------------------------------
+
+for target in diff_list :
+  directory = target.split('/')
+  file_name = directory.pop()
+  directory = './dist/' + '/'.join(directory)
+
+  if os.path.exists(directory) :
+    if os.path.isfile(target) :
+      shutil.copyfile(target, directory + '/' + file_name)
+  else :
+    if os.path.isfile(target) :
+      os.makedirs(directory)
+      shutil.copyfile(target, directory + '/' + file_name)
